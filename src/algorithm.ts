958c2ed1ea1eb8b6d87d5c859aaeb860777412ee
Florian Walter <nairolf.retlaw@gmail.com>
2020-02-20 21:23:19 +0100

    added customizable ratio
    added not working total point calculation

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -14,9 +14,9 @@ function sortBooksByScore(books: number[], scores: number[]) {
     return sorted.map(obj => obj.idx);
 }
 
-function rankLibrary(library: Library, book_scores: number[], total_days: number): RankedLibrary|null {
+function rankLibrary(library: Library, book_scores: number[], total_days: number, ratio: number): RankedLibrary | null {
     const percentage = library.num_days_for_signup / total_days;
-    if (percentage > 0.3) {
+    if (percentage > ratio) {
         return null;
     }
 
@@ -35,12 +35,18 @@ function rankLibrary(library: Library, book_scores: number[], total_days: number
             total_days--;
 
             if (total_days === 0) {
-                return { library, total_days, points, sorted_books, scanable_books };
+                return {
+                    library,
+                    total_days,
+                    points: points / library.num_days_for_signup,
+                    sorted_books,
+                    scanable_books
+                };
             }
         }
     }
 
-    return { library, total_days, points, sorted_books, scanable_books };
+    return { library, total_days, points: points / library.num_days_for_signup, sorted_books, scanable_books };
 }
 
 function reRankLibrary(input: Library, scores: number[]): RankedLibrary {
@@ -48,13 +54,56 @@ function reRankLibrary(input: Library, scores: number[]): RankedLibrary {
     for (const book of input.books) {
         total += scores[book];
     }
-    return { library: input, points: total };
+    return { library: input, points: total / input.num_days_for_signup };
 }
 
-export default function (input: Reader): Writer {
+interface Test {
+    library: Library
+    scanned_books: number,
+}
+
+function calculateTotal(libs: Library[], scores: number[], total_days: number): number {
+    const processed = new Array<Test>();
+    let points = 0;
+
+    let currentSignUpIndex = 0;
+    let currentSignUpLib: Library = null;
+    let currentSignUpLeft = 0;
+
+    for (let i = 0; i < total_days; i++) {
+        if (currentSignUpLeft === 0) {
+            if (currentSignUpLib !== null) {
+                processed.push({ library: currentSignUpLib, scanned_books: 0 });
+            }
+
+            if (currentSignUpLeft !== libs.length) {
+                currentSignUpLib = libs[currentSignUpIndex++];
+                currentSignUpLeft = currentSignUpLib.num_days_for_signup;
+            }
+        }
+        currentSignUpLeft--;
+
+        for (let j = 0; j < processed.length; j++) {
+            const lib = processed[j];
+
+            for (let k = 0; k < lib.library.num_books_per_day; i++) {
+                if (lib.scanned_books >= lib.library.books.length) {
+                    break;
+                }
+
+                const book = lib.library.books[lib.scanned_books++];
+                points += scores[book];
+            }
+        }
+    }
+
+    return points;
+}
+
+function runWithRatio(input: Reader, ratio: number): Writer {
     const rankedLibraries = new Array<RankedLibrary>();
     for (const library of input.libraries) {
-        const ranked = rankLibrary(library, input.scores, input.num_days);
+        const ranked = rankLibrary(library, input.scores, input.num_days, ratio);
         if (ranked === null) {
             continue;
         }
@@ -69,7 +118,7 @@ export default function (input: Reader): Writer {
     for (const ranked of sortedRankedLibraries) {
         const books = [];
         for (const book of ranked.scanable_books) {
-            if (scannedBooks[book]) {
+            if (scannedBooks[book] === true) {
                 continue;
             }
 
@@ -83,7 +132,7 @@ export default function (input: Reader): Writer {
 
         const lib: Library = {
             ...ranked.library,
-            books: books,
+            books: books
         };
 
         const reRanked = reRankLibrary(lib, input.scores);
@@ -94,4 +143,20 @@ export default function (input: Reader): Writer {
     const libraries: Array<Library> = sortedReRankedLibraries.map(lib => lib.library);
 
     return { libraries };
+}
+
+export default function (input: Reader): Writer {
+    let output = runWithRatio(input, 0.15);
+    // let max = 0;
+    //
+    // for (let ratio = 0.1; ratio < 1; ratio += 0.1) {
+    //     const res = runWithRatio(input, ratio);
+    //     const total = calculateTotal(res.libraries, input.scores, input.num_days);
+    //
+    //     if (total > max) {
+    //         output = res;
+    //     }
+    // }
+
+    return output;
 }
