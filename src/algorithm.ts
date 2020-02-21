a1c8dc6302a5580f483512fe0888c172fa7ef4de
Philip Breuer <philip.breuer@pm.me>
2020-02-21 02:41:52 +0100

    Second try

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -1,6 +1,7 @@
 import {Library, Reader, Writer} from './index';
 
 export default function (input: Reader): Writer {
+    // const librarySignUpDaysMax = input.libraries.reduce((p, l) => l.num_days_for_signup > 0, input.libraries[0]);
     let libraries: Library[];
     let signUpWeight = 1;
     let throughputWeight = 1;
@@ -11,48 +12,60 @@ export default function (input: Reader): Writer {
 
     // while (true) {
         libraries = input.libraries
-            .sort((a, b) => compare(b) - compare(a))
+            .sort((a, b) => weigh(b) - weigh(a))
             .map(l => ({
                 ...l,
                 books: l.books.sort((a, b) => b - a)
             }) as Library);
 
 
-        function compare(a: Library) {
+        function weigh(a: Library) {
             const signUp = a.num_days_for_signup * signUpWeight; // less is better
             const throughput = a.num_books_per_day * throughputWeight; // higher is better
-            const scoreAverage = a.books // higher is better
+            const scoreMin = Math.min(...a.books);
+            const scoreMax = Math.max(...a.books);
+            let scoreAverage = a.books // higher is better
                 .sort((a, b) => input.scores[b] - input.scores[a])
-                .reduce((p, a) => p + a, 0)
-                / a.books.length * scoreAverageWeight;
+                .reduce((p, a) => p + a, 0);
+            scoreAverage = (scoreAverage - scoreMin) / scoreMax / a.books.length * scoreAverageWeight
             const duplicates = a.books.filter(b => !input.libraries
-                .find(l => l.books.indexOf(b)))
+                .find(l => l.books.indexOf(b) === -1))
                 * duplicateWeight;
-            return (-signUp + throughput + scoreAverage + -duplicates) / 4;
+            // let amount = input.num_days - a.books.length;
+            // if (amount < 0) amount = 0;
+            // amount = amount / input.num_days * amountBooksWeight;
+
+            // console.log(duplicates, (-signUp + throughput + scoreAverage + -duplicates) / 4)
+            return (-signUp + throughput + scoreAverage + -duplicates /* + amount */) / 4;
         }
 
-        const books = new Set<number>();
+        const books = [];
         let points = 0;
         let availableDays = input.num_days;
         libraries = libraries.map(l => {
-            if (availableDays > 0) {
+            if (availableDays - l.num_days_for_signup > 0) {
                 availableDays -= l.num_days_for_signup;
-                return {
+                let counter = 0;
+                l = {
                     id: l.id,
                     books: l.books.filter(b => {
-                        if (!books.has(b)) {
-                            points += input.scores[b];
-                            return true;
+                        counter++;
+                        if (counter <= l.num_books_per_day * availableDays) {
+                            if (books.indexOf(b) === -1) {
+                                points += input.scores[b];
+                                books.push(b);
+                                return true;
+                            }
                         }
-                        books.add(b);
-                    }).slice(0, l.num_books_per_day * availableDays)
+                    })
                 } as Library;
+                return l
             }
         }).filter(l => !!l).filter(l => !!l.books.length);
 
         if (points > max) {
             max = points;
-            console.log('BETTER: ' + points);
+            console.log('BEST: ' + points.toLocaleString());
         }
     // }
 
